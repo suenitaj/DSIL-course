c048072b23cf15631782c072f76904a4af5068f6
suenitaj <118204210+suenitaj@users.noreply.github.com>
2022-11-14 11:46:00 -0500

    Create Jupyter Notebook.py
    Adding new file

---
+++ Jupyter Notebook.py
@@ -0,0 +1,1 @@
+https://eu-de.dataplatform.cloud.ibm.com/analytics/notebooks/v2/e6a965e1-8bdc-4d80-8f65-f1a009064c69/view?access_token=1b73b2c0f75a57ae71783d9bef4d2fcc4c2bb7ee08daa98f70a2c6a30e42115b
